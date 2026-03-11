807a08aed1b8d868221f1b54a20a0ea438073577
deniaverkade <9024406@student.zadkine.nl>
2026-03-11 12:02:03 +0100

    opdracht 2 ( niet gelukt)

--- src/opdracht-02/passwordValidator.test.js
+++ src/opdracht-02/passwordValidator.test.js
@@ -1,7 +1,7 @@
 import { validatePassword } from './passwordValidator';
 
 describe('Password Validator', () => {
-  
+
   // VOORBEELD - Deze test is al ingevuld
   test('VOORBEELD: geldig wachtwoord geeft isValid true', () => {
     const result = validatePassword('Welkom123');
@@ -12,23 +12,38 @@ describe('Password Validator', () => {
   test('te kort wachtwoord geeft error', () => {
     // TODO: Test of 'Test1' invalid is
     // Hint: check of isValid false is
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+    const password = "Test1";
+    const result = validatePassword(password);
+    expect(password.length).toBeGreaterThanOrEqual(8);
+    expect(result.isValid).toBe(false);
+    expect(result.errors).toEqual(['Minimaal 8 karakters']);
   });
 
   test('wachtwoord zonder nummer geeft error', () => {
     // TODO: Test of 'Welkomtestje' invalid is
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+    const password = "Welkomtestje";
+    const result = validatePassword(password);
+    expect(password).toMatch(/[0-9]/);
+    expect(result.isValid).toBe(false);
+    expect(result.errors).toEqual(['Moet minimaal 1 nummer bevatten']);
   });
 
   test('wachtwoord zonder hoofdletter geeft error', () => {
     // TODO: Test of 'welkom123' invalid is
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+    const password = "welkom123";
+    const result = validatePassword(password);
+    expect(result.password).toMatch(/[A-Z]/);
+    expect(result.isValid).toBe(false);
+    expect(result.errors).toEqual(['Moet minimaal 1 hoofdletter bevatten']);
   });
 
   test('errors array bevat juiste foutmeldingen', () => {
     // TODO: Test of 'test' meerdere errors teruggeeft
     // Hint: gebruik .toContain() om te checken of een error in de array zit
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+    const result = validatePassword("test");
+    expect(result.errors).toContain(['Minimaal 8 karakters', 'Moet minimaal 1 nummer bevatten', 'Moet minimaal 1 hoofdletter bevatten'])
   });
 
 });
+
+//Alles met het wachtwoord geeft undefined terug, ik weet niet hoe ik dit moet oplossen. 
