42eb91cb205e9341ce8302d4931600d213147749
deniaverkade <9024406@student.zadkine.nl>
2026-04-14 17:26:09 +0200

    6

--- src/opdracht-06/ProfileCard.test.jsx
+++ src/opdracht-06/ProfileCard.test.jsx
@@ -14,19 +14,25 @@ describe('ProfileCard', () => {
     // TODO: Render ProfileCard met name="Piet" en age={30}
     // TODO: Check of tekst "Leeftijd: 30" op het scherm staat
     // Hint: gebruik screen.getByText()
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+    render(<ProfileCard name="Piet" age={30} />);
+    const ageText = screen.getByText('Leeftijd: 30');
+    expect(ageText).toBeInTheDocument();
   });
 
   test('toont bio als die er is', () => {
     // TODO: Render ProfileCard met bio="Ik hou van programmeren"
     // TODO: Check of die tekst op het scherm staat
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+    render(<ProfileCard name="Anna" bio="Ik hou van programmeren" />);
+    const bioText = screen.getByText('Ik hou van programmeren');
+    expect(bioText).toBeInTheDocument();
   });
 
   test('toont fallback tekst als bio ontbreekt', () => {
     // TODO: Render ProfileCard ZONDER bio prop
     // TODO: Check of "Geen bio beschikbaar" op het scherm staat
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+   render(<ProfileCard name="Klaas" />);
+    const fallbackText = screen.getByText('Geen bio beschikbaar');
+    expect(fallbackText).toBeInTheDocument();
   });
 
 });
